289dd3472d4006d3da4cfb6aa5acbb71930e87cb
Kristel Nieuwenhuys <kristel@kramphub.com>
2023-11-26 17:07:45 +0100

    prepare day01

---
+++ src/main/kotlin/day01/P01_Solver.kt
@@ -0,0 +1,19 @@
+package day01
+
+import base.BaseSolver
+import base.TestOrReal
+
+fun main(args: Array<String>) {
+    P01_Solver().solve(TestOrReal.TEST)
+}
+
+class P01_Solver : BaseSolver() {
+
+    override fun solvePart1(inputLines: List<String>, testOrReal: TestOrReal): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun solvePart2(inputLines: List<String>, testOrReal: TestOrReal): Any {
+        TODO("Not yet implemented")
+    }
+}
---
+++ src/main/kotlin/dayxx_template/Pxx_Solver.kt
@@ -0,0 +1,23 @@
+package dayxx_template
+
+import base.BaseSolver
+import base.TestOrReal
+
+fun main(args: Array<String>) {
+    P00_Solver().solve(TestOrReal.TEST)
+}
+
+class P00_Solver : BaseSolver() {
+    override fun solvePart1(inputLines: List<String>, testOrReal: TestOrReal): Any{
+        return inputLines.size
+    }
+
+    override fun solvePart2(inputLines: List<String>, testOrReal: TestOrReal): Any {
+        var context = mutableMapOf<String, Any>(Pair("1", "100"))
+        context.put("someKey", "someValue")
+
+        return Pair(inputLines.size * 10, context)
+    }
+}
+
+
