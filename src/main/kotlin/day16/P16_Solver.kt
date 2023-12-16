e378598a751db5173d0a99101dc176230be771c9
Kristel Nieuwenhuys <kristel@kramphub.com>
2023-12-16 20:59:44 +0100

    prepare day 16, 17

---
+++ src/main/kotlin/day16/P16_Solver.kt
@@ -0,0 +1,24 @@
+package day16
+
+import base.BaseSolver
+import base.INPUT_VARIANT
+
+fun main(args: Array<String>) {
+    P16_Solver().solve(INPUT_VARIANT.EXAMPLE)
+}
+
+class P16_Solver : BaseSolver() {
+
+    override fun getPuzzleName(): String {
+        return "beam of light"
+    }
+    override fun solvePart1(inputLines: List<String>, inputVariant: INPUT_VARIANT): Any{
+        return "TODO"
+    }
+
+    override fun solvePart2(inputLines: List<String>, inputVariant: INPUT_VARIANT): Any {
+        return "TODO"
+    }
+}
+
+
---
+++ src/main/kotlin/day17/P17_Solver.kt
@@ -0,0 +1,24 @@
+package day17
+
+import base.BaseSolver
+import base.INPUT_VARIANT
+
+fun main(args: Array<String>) {
+    P17_Solver().solve(INPUT_VARIANT.EXAMPLE)
+}
+
+class P17_Solver : BaseSolver() {
+
+    override fun getPuzzleName(): String {
+        return "<unnamed>"
+    }
+    override fun solvePart1(inputLines: List<String>, inputVariant: INPUT_VARIANT): Any{
+        return "TODO"
+    }
+
+    override fun solvePart2(inputLines: List<String>, inputVariant: INPUT_VARIANT): Any {
+        return "TODO"
+    }
+}
+
+
