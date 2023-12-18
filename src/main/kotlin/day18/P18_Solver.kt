fbb36db4314edf560ed54e4d1680c05aef20773d
Kristel Nieuwenhuys <kristel@kramphub.com>
2023-12-18 21:31:40 +0100

    try out day 18, part 2

--- src/main/kotlin/day18/P18_Solver.kt
+++ src/main/kotlin/day18/P18_Solver.kt
@@ -2,13 +2,14 @@ package day18
 
 import base.BaseSolver
 import base.INPUT_VARIANT
+import base.Part
 import util.grid.*
 import java.lang.Error
 import kotlin.IllegalArgumentException
 import kotlin.math.min
 
 fun main(args: Array<String>) {
-    P18_Solver().solve(INPUT_VARIANT.REAL)
+    P18_Solver().solve(INPUT_VARIANT.EXAMPLE, Part.PART1)
 }
 
 class P18_Solver : BaseSolver() {
@@ -20,7 +21,7 @@ class P18_Solver : BaseSolver() {
     // answer: 68115
     override fun solvePart1(inputLines: List<String>, inputVariant: INPUT_VARIANT): Any{
         val instructions = inputLines.map {
-            val (directionRaw, distanceRaw, colorRaw) = it.split(" ")
+            val (directionRaw, distanceRaw, _) = it.split(" ")
             val direction = when (directionRaw) {
                 "U" -> Direction.NORTH
                 "D" -> Direction.SOUTH
@@ -29,8 +30,7 @@ class P18_Solver : BaseSolver() {
                 else -> throw IllegalArgumentException("$directionRaw is an unknown direction")
             }
             val distance = distanceRaw.toInt()
-            val color = colorRaw.substring(1, colorRaw.length-2)
-            Instruction(direction, distance, color)
+            Instruction(direction, distance)
         }
         var columnCount = 0
         var maxColumns = 0
@@ -66,7 +66,7 @@ class P18_Solver : BaseSolver() {
                 cursor.move(it.direction, it.distance, true, '#')
             }
 
-            grid.print()
+//            grid.print()
 
             markInner(grid, cursor.path.reversed(), '#','%')
         } catch(e:Error) {
@@ -79,7 +79,62 @@ class P18_Solver : BaseSolver() {
     }
 
     override fun solvePart2(inputLines: List<String>, inputVariant: INPUT_VARIANT): Any {
-        return "TODO"
+        val instructions = inputLines.map {
+            val distanceHex = it.substring(it.length-7, it.length-2)
+            val distance = Integer.decode("0x$distanceHex")
+            val direction = when (it[it.length-2]) {
+                '3' -> Direction.NORTH
+                '1' -> Direction.SOUTH
+                '2' -> Direction.WEST
+                '0' -> Direction.EAST
+                else -> throw IllegalArgumentException("${it[it.length-2]} is an unknown direction")
+            }
+            Instruction(direction, distance)
+        }
+        var columnCount = 0
+        var maxColumns = 0
+        var minColumns = 0
+        instructions
+            .filter{it.direction == Direction.WEST || it.direction == Direction.EAST}
+            .forEach {
+                columnCount += if (it.direction == Direction.EAST) it.distance else -it.distance
+                maxColumns = maxOf(maxColumns, columnCount)
+                minColumns = minOf(minColumns, columnCount)
+            }
+
+        var rowCount = 0
+        var maxRows = 0
+        var minRows = 0
+        instructions
+            .filter{it.direction == Direction.NORTH || it.direction == Direction.SOUTH}
+            .forEach{
+                rowCount += if (it.direction == Direction.SOUTH) it.distance  else -it.distance
+                maxRows = maxOf(maxRows, rowCount)
+                minRows = minOf(minRows, rowCount)
+            }
+
+        println("maxRows: $maxRows, maxColumns: $maxColumns")
+        println("minRows: $minRows, minColumns: $minColumns")
+        println("dimensions: ${maxRows-minRows+1}, ${maxColumns-minColumns+1}")
+
+        val grid = Grid2D<Char>(maxRows-minRows+1, maxColumns-minColumns+1, '.')
+        try {
+            val cursor = Cursor(grid, Coordinate(-minRows, -minColumns), instructions[0].direction, CursorPathStrategy.FULL_PATH, CursorGridStrategy.NO_VISITED)
+            instructions.forEach{
+                cursor.move(it.direction, it.distance, true, '#')
+            }
+
+//            grid.print()
+
+            println("path size: ${cursor.path.size}")
+            markInner(grid, cursor.path.reversed(), '#','%')
+        } catch(e:Error) {
+            throw e
+        } finally {
+//            grid.print()
+        }
+
+        return grid.count('#') + grid.count('%')
     }
 
     fun markInner(grid:Grid2D<Char>, path:List<VisitedCoordinate>, borderValue: Char, innerValue:Char) {
@@ -145,5 +200,5 @@ class P18_Solver : BaseSolver() {
     }
 }
 
-data class Instruction(val direction:Direction, val distance:Int, val color:String)
+data class Instruction(val direction:Direction, val distance:Int)
 
