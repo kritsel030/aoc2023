4e6feb5060c871074cbb117ad7363b688be463a2
Kristel Nieuwenhuys <kristel@kramphub.com>
2023-12-24 17:21:31 +0100

    day 13, part 2

--- src/main/kotlin/day13/P13_Solver.kt
+++ src/main/kotlin/day13/P13_Solver.kt
@@ -9,7 +9,7 @@ import java.lang.IllegalArgumentException
 import java.lang.Integer.min
 
 fun main(args: Array<String>) {
-    P13_Solver().solve(INPUT_VARIANT.EXAMPLE)
+    P13_Solver().solve(INPUT_VARIANT.REAL)
 }
 
 class P13_Solver : BaseSolver() {
@@ -36,14 +36,10 @@ class P13_Solver : BaseSolver() {
         }
    }
 
+    // answer: 34536
     override fun solvePart2(inputLines: List<String>, inputVariant: INPUT_VARIANT): Any {
-        val pattern2 = chunkInput(inputLines)[1]
-        val grid2= Grid2DFactory.initCharGrid(pattern2, 1)
-        println("col 2: ${grid2.getValues(ORIENTATION.VERTICAL, 2)}")
-        println("col 5: ${grid2.getValues(ORIENTATION.VERTICAL, 5)}")
-        println("differences: ${differences(grid2.getValues(ORIENTATION.VERTICAL, 2), grid2.getValues(ORIENTATION.VERTICAL, 5))}")
-
         return chunkInput(inputLines).sumOf {pattern ->
+//            println("***********************************************")
             // we use a 1-based index for this puzzle's grid
             // meaning that the index number for the first column/row is 1 and not 0
             val grid = Grid2DFactory.initCharGrid(pattern, 1)
@@ -77,7 +73,7 @@ class P13_Solver : BaseSolver() {
 
                     // now check if each pair of rows/colums within the mirror span is identical
                     // (we count the pairs which AREN'T equal and check if that count is 0)
-                    val mirrorFound = isMirrorPresent(grid, it, orientation, mirrorSpan)
+                    val mirrorFound = isMirrorPresent(grid, it, null, orientation, mirrorSpan)
                     //                    println("mirror found: $mirrorFound")
                     mirrorFound
                 } else {
@@ -88,12 +84,18 @@ class P13_Solver : BaseSolver() {
     }
 
     private fun findMirrorsForOrientation2(grid: Grid2D<Char>, orientation: ORIENTATION): List<Int> {
+//        println("find2 $orientation")
         val size = grid.size(orientation)
 
         // this puzzle's grid uses a 1-based index, so we start at 1
-        val potentialMirrorOuterIndices = (1 .. size)
+        // find row/column indices and their smudge-matching row/column index counterparts
+        // results in a map with
+        // - key: a row or column index
+        // - value: the list of row/column indices whose row/column values only differ in 1 position with the
+        //   values in the key row/column
+        val potentialMirrorSmudgeIndices = (1 until size)
             .map { index1 ->
-                index1 to (1..size)
+                index1 to (2..size)
                     .filter { index2 ->
                         index1 != index2
                                 && index1 < index2
@@ -103,17 +105,18 @@ class P13_Solver : BaseSolver() {
             }
             .toMap().filter { (key, value) -> value.isNotEmpty() }
 
-        println("[$orientation] potentialMirrorOuterIndices: $potentialMirrorOuterIndices")
+//        println("[$orientation] potentialMirrorSmudgeIndices: $potentialMirrorSmudgeIndices")
 
-        val indices = potentialMirrorOuterIndices.map {(index1, others) ->
+        val indices = potentialMirrorSmudgeIndices.map { (index1, others) ->
             others.map { otherIndex ->
-                val mirrorSpan = (otherIndex - index1 - 1) / 2
-                val centerIndex = index1 + mirrorSpan
-                if (isMirrorPresent(grid, centerIndex, orientation, mirrorSpan-1)) centerIndex else -1
+//                val mirrorSpan = (otherIndex - index1 - 1) / 2
+                val centerIndex = index1 + (otherIndex - index1 - 1) / 2
+                val mirrorSpan = min(centerIndex, size-centerIndex)
+                if (isMirrorPresent(grid, centerIndex, index1, orientation, mirrorSpan)) centerIndex else -1
             }.filter { it >= 0 }
         }.flatten()
 
-        println("findMirrorsForOrientation2(grid, $orientation) = $indices")
+//        println("findMirrorsForOrientation2(grid, $orientation) = $indices")
         return indices
     }
 
@@ -129,14 +132,16 @@ class P13_Solver : BaseSolver() {
     private fun isMirrorPresent(
         grid: Grid2D<Char>,
         startIndex: Int,
+        skipIndex: Int?,
         orientation: ORIENTATION,
         mirrorSpan: Int
     ): Boolean {
-        val mirrorFound = (0 until mirrorSpan).count { indexDelta ->
-    //                        print("compare $orientation indices ${it - indexDelta} and ${it + 1 + indexDelta}: ")
+        val mirrorFound = (0 until mirrorSpan)
+            .count { indexDelta ->
+//                print("compare $orientation indices ${startIndex - indexDelta} and ${startIndex + 1 + indexDelta}: ")
             val result =
-                grid.getValues(orientation, startIndex - indexDelta).equals(grid.getValues(orientation, startIndex + 1 + indexDelta))
-    //                        println(result)
+                startIndex - indexDelta == skipIndex || grid.getValues(orientation, startIndex - indexDelta).equals(grid.getValues(orientation, startIndex + 1 + indexDelta))
+//            println(result)
             !result
         } == 0
         return mirrorFound
